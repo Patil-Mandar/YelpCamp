03e282012694c4984a10c47b8689a53348d4b29b
Mandar <mvp23402@gmail.com>
2022-03-18 00:37:22 +0530

    added start script in json

--- app.js
+++ app.js
@@ -105,8 +105,9 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('campground/error',{err})
 })
 
-app.listen(3000,() => {
-    console.log("Connnected to Port 3000")
+const port = process.env.PORT || '3000'
+app.listen(port,() => {
+    console.log(`Connnected to Port ${port}`)
 })
 
 
